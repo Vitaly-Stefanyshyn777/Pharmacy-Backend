d2cd7678efdbc983aa04065b56c1ebbe4e281b56
Vitaly-Stefanyshyn777 <167921099+Vitaly-Stefanyshyn777@users.noreply.github.com>
2025-05-06 09:25:11 +0300

    заміна bcrypt на bcryptjs

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import createHttpError from 'http-errors';
 import { UsersCollection } from '../db/models/user.js';
 import { SessionsCollection } from '../db/models/session.js';
